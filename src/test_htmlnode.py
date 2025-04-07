22c0a8e3baea88ab31a3bf1c8736cc104aed60b3
FriendlyStepBro <logicye@proton.me>
2025-04-07 11:36:27 +1000

    Finished the HTMLNode as well as its associated tests and removed environments variables from the repository

---
+++ src/test_htmlnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html_exception(self):
+         with self.assertRaises(NotImplementedError):
+             test_obj = HTMLNode()
+             test_obj.to_html()
+        
+    def test_props_to_html(self):
+        node = HTMLNode('p', 'test', None, {'href': 'https://google.com', 'target': '_blank',})
+        text = ' href="https://google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), text)
+
+    def test_to_string(self):
+        childnodes = [
+            HTMLNode('a', 'testing_a_child', None, {'href': 'https://childOne.com', 'target': '_self'}),
+            HTMLNode('b', 'test_bold_text_child', None, {'link': 'ftp://192.168.50.12'})
+        ]
+        node = HTMLNode('p', 'test', childnodes, {'href': 'https://google.com', 'target': '_blank',})
+        text =  "HTMLNode(p, test, ["
+        text += "HTMLNode(a, testing_a_child, None, {'href': 'https://childOne.com', 'target': '_self'}), "
+        text += "HTMLNode(b, test_bold_text_child, None, {'link': 'ftp://192.168.50.12'})"
+        text += "], {'href': 'https://google.com', 'target': '_blank'})"
+        self.assertEqual(str(node), text)
+    
+if __name__ == "__main__":
+    unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,27 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_full_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://google.com")
+        self.assertEqual(node, node2)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://google.com")
+        self.assertNotEqual(node, node2)
+    
+    def test_to_string_no_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        text = "TextNode(This is a text node, bold, None)"
+        self.assertEqual(str(node), text)
+
+    def test_to_string(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://google.com")
+        text = "TextNode(This is a text node, bold, https://google.com)"
+        self.assertEqual(str(node), text)
+        
+
 
 if __name__ == "__main__":
     unittest.main()
